44b1dc84d603903185def3fce5b0ab44b53f0a50
prakashwaddar628 <prakashwaddar628@gmail.com>
2026-05-05 10:49:27 +0530

    added backend CORS

--- api/main.py
+++ api/main.py
@@ -1,7 +1,16 @@
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 from api.db import cursor
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
