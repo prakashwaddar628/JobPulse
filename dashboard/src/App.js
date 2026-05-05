2c3fc6e311fa3958d05c5a1e52f8603c0abf7b1d
prakashwaddar628 <prakashwaddar628@gmail.com>
2026-05-05 10:39:37 +0530

    frontend initilized

---
+++ dashboard/src/App.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { BarChart, Bar, XAxis, YAxis, Tooltip } from "recharts";
+
+
+function App() {
+  const [skills, setSkills] = useState([])
+
+  useEffect(() => {
+    axios.get("http://127.0.0.1:8000/skills")
+    .then(res => {
+      const data = Object.entries(res.data).map(([key, value]) => ({
+        skill: key,
+        count: value
+      }));
+      setSkills(data);
+    });
+  }, []);
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>🔥 Job Market Skill Trends</h2>
+
+      <BarChart width={600} height={300} data={skills}>
+        <XAxis dataKey="skill" />
+        <YAxis />
+        <Tooltip />
+        <Bar dataKey="count" />
+      </BarChart>
+    </div>
+  )
+}
+
+export default App;
